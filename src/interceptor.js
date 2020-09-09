c35260d4dfc20e4ade0ae188db70c60b5ba23f79
Jiajun Yan <domo36k@gmail.com>
2020-09-09 09:47:48 +0800

    fix: Set webpack target to node so axios won't treat all env to browser

--- src/interceptor.js
+++ src/interceptor.js
@@ -1,20 +1,4 @@
-import _axios from 'axios';
-import xhrAdapter from 'axios/lib/adapters/xhr';
-import httpAdapter from 'axios/lib/adapters/http';
-
-function getDefaultAdapter() {
-  let adapter;
-  if (typeof XMLHttpRequest !== 'undefined') {
-    // For browsers use XHR adapter
-    adapter = xhrAdapter;
-  } else if (typeof process !== 'undefined') {
-    // For node use HTTP adapter
-    adapter = httpAdapter;
-  }
-  return adapter;
-}
-
-const axios = _axios.create({ adapter: getDefaultAdapter() });
+import axios from 'axios';
 
 axios.interceptors.request.use((config) => {
   config.timeout = 40 * 1000; // timeout 40s
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   mode: 'production',
+  target: 'node',
   entry: './src/index.js',
   output: {
     filename: './index.js',
