9f56516db47a39d23f23937842258e1f5236d344
Jiajun Yan <yanjiajun@ifanr.com>
2021-03-21 01:12:56 +0800

    refactor: Change invoke to call in cloud functions

--- src/cloud-function.ts
+++ src/cloud-function.ts
@@ -9,22 +9,22 @@ import config from './config';
  */
 export default class CloudFunction {
   /**
-   * Invoke cloud function
+   * Call cloud function
    * @returns cloud function callback result
    */
-  static async invoke(name: string, params = {}): Promise<void> {
+  static async call(name: string, params = {}): Promise<void> {
     if (!name) {
       throw new CurveError(600);
     }
 
-    // Cloud functions cannot be invoked in Node environment
+    // Cloud functions cannot be called in Node environment
     if (config.WITH_NODE) {
       throw new CurveError(603);
     }
 
     return await API.cloudFunction.invoke({
       params: { name },
-      body: params,
+      data: params,
     });
   }
 }
